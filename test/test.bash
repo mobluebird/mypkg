fe1127657505eaf488ebe4d0da304a75186ff787
mo <mo@bluebird.com>
2025-12-25 10:31:02 +0900

    Update

--- test/test.bash
+++ test/test.bash
@@ -8,15 +8,13 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
+
 timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-#grep -q 'UTC'  /tmp/mypkg.log
-#grep -q 'JD'   /tmp/mypkg.log
-#grep -q 'GMST' /tmp/mypkg.log
-#grep -q 'LST'  /tmp/mypkg.log
+grep -q '\[listener'  /tmp/mypkg.log
+grep -q 'UTC'  /tmp/mypkg.log
+grep -q 'JD'   /tmp/mypkg.log
+grep -q 'GMST' /tmp/mypkg.log
+grep -q 'LST'  /tmp/mypkg.log
 
-grep -q 'UTC *:' "$log"
-grep -q 'JD  *:' "$log"
-grep -q 'GMST *:' "$log"
-grep -q 'LST *(Tokyo):' "$log"
 
