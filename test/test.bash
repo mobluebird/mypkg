7c753efa8ff1dbce59092afe0811d0428651d4aa
mo <mo@bluebird.com>
2025-12-25 10:44:24 +0900

    Update

--- test/test.bash
+++ test/test.bash
@@ -17,6 +17,11 @@ timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 #grep -q 'GMST' /tmp/mypkg.log
 #grep -q 'LST'  /tmp/mypkg.log
 
+ros2 topic list | grep -q '^/utc_time$'
+ros2 topic list | grep -q '^/julian_day$'
+ros2 topic list | grep -q '^/gmst$'
+ros2 topic list | grep -q '^/lst$'
+
 grep -q '\[listener' /tmp/mypkg.log
 grep -q 'UTC :'  /tmp/mypkg.log
 grep -q 'JD  :'  /tmp/mypkg.log
