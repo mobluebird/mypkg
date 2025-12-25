9ed7c5d375712b07107dc7f2a0dca4c4d0af91e3
mo <mo@bluebird.com>
2025-12-25 10:54:19 +0900

    Update

--- test/test.bash
+++ test/test.bash
@@ -9,19 +9,23 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
+# talker 
+timeout 20 ros2 run mypkg talker > /tmp/talker.log &
+sleep 3
+ros2 topic echo --once /utc_time > /tmp/utc.log
+grep -q 'data:' /tmp/utc.log
+pkill -f 'ros2 run mypkg talker' || true
+
+# launch
 timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-#grep -q '\[listener'  /tmp/mypkg.log
-#grep -q 'UTC'  /tmp/mypkg.log
-#grep -q 'JD'   /tmp/mypkg.log
-#grep -q 'GMST' /tmp/mypkg.log
-#grep -q 'LST'  /tmp/mypkg.log
-
+# topic 存在確認
 ros2 topic list | grep -q '^/utc_time$'
 ros2 topic list | grep -q '^/julian_day$'
 ros2 topic list | grep -q '^/gmst$'
 ros2 topic list | grep -q '^/lst$'
 
+# listener 出力確認
 grep -q '\[listener' /tmp/mypkg.log
 grep -q 'UTC :'  /tmp/mypkg.log
 grep -q 'JD  :'  /tmp/mypkg.log
